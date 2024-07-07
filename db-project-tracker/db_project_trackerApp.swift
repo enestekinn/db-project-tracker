c1b272ceae11d234c425f0f54ef41f398e02fa5a
Enes Tekin <enestekin_@hotmail.com>
2024-07-07 16:51:52 +0300

    Initial Commit

---
+++ db-project-tracker/db_project_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  db_project_trackerApp.swift
+//  db-project-tracker
+//
+//  Created by Enes Tekin on 7.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct db_project_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
